d7c8dbb4b7e9f0ae7b78fc9b6aac0459dde4c037
Nadia Aizpeolea <nadia.aizpeolea@gmail.com>
2023-04-09 11:48:03 -0300

    Sesiones

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,11 +1,16 @@
-import '../../styles/components/layout/Header.css';
-import React from 'react';
-const Header= (props)=>{
-    return (
-        <div> Header </div>
-    );
+import React from "react";
+
+import "../../styles/components/layout/Header.css";
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="holder">
+        <img src={process.env.PUBLIC_URL + "/img/logo.jpg"} />
+        <h1>Plantas y Otras Hierbas</h1>
+      </div>
+    </header>
+  );
 };
-
-
 
 export default Header;
