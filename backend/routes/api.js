b257fe0f25217b179671c18cd48fb2ce37b0963d
Nadia Aizpeolea <nadia.aizpeolea@gmail.com>
2023-05-10 19:26:05 -0300

    cargado contacto

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -25,5 +26,31 @@ router.get('/novedades', async function (req, res, next) {
    });
    res.json(novedades);
 });
+
+router.post('/contacto', async (req, res)=>{
+    const mail = {
+        to: 'nadia.aizpeolea@gmail.com',
+        subject: "contacto web",
+        html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} 
+        <br> Además, hizo el siguiente comentario: ${req.body.mensaje}</br> su telefono es : ${req.body.telefono}`
+    }
+    const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth:{
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+    error: false, 
+    message: 'mensaje enviado'
+    });
+
+});
+
 
 module.exports = router;
